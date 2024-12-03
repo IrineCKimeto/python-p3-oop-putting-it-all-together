43252e996092d09349e8f6b09b52192981e81e9c
IrineCKimeto <irenekimetoc@gmail.com>
2024-12-03 14:52:47 +0300

    adds lib/book.py

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author} (Genre: {self.genre})"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def repair(self):
+        self.condition = "Like New"
+
+    def __str__(self):
+        return f"Shoe: {self.brand}, Size: {self.size}, Condition: {self.condition}"
