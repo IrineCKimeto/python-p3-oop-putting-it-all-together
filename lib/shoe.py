9f8cfc9a3e21d467f7cf7a0ba52ae4ee4c21e2a8
IrineCKimeto <irenekimetoc@gmail.com>
2024-12-03 15:25:26 +0300

    adds lib/book.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,13 +1,23 @@
-#!/usr/bin/env python3
-
 class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
-        self.size = size
+        self.size = size  
         self.condition = "New"
 
-    def repair(self):
-        self.condition = "Like New"
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
 
     def __str__(self):
         return f"Shoe: {self.brand}, Size: {self.size}, Condition: {self.condition}"
